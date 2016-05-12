8ca82f93897e7c4c2fa991e30b33dccfc2205f98
nikolan92 <92nikolan@gmail.com>
2016-05-12 21:30:37 +0200

    Dodati soceti

--- Socket.IO/socket.js
+++ Socket.IO/socket.js
@@ -17,7 +17,7 @@ io.on('connect', function(socket){
 
   socket.on("myCurrentLocation", function(data){
     userData[socket.id] = data;
-
+    //console.log(userData[socket.id].lat);
     console.log("User send data of his location:" + JSON.stringify(userData[socket.id]));
 
     // // //test
@@ -36,7 +36,7 @@ io.on('connect', function(socket){
 
     var path = __dirname;
     path = path.replace("Socket.IO","public\\images\\temp.jpeg");
-    console.log(path);
+    //console.log(path);
     fs.writeFile(path, finalImage.data, "binary", function(err) {
       if(err) {
         console.log(err);
@@ -44,10 +44,27 @@ io.on('connect', function(socket){
       } else {
         console.log("The file was saved!");
 
-        //before emit chack to which user to send picture
-        //io.emit('newPictureAdded', "/images/temp.jpeg");//emit to all user for now
-        socket.broadcast.emit('newPictureAdded', "/images/temp.jpeg");
-        socket.emit("success", "/images/temp.jpeg");
+        socket.emit("success", "/images/temp.jpeg");//return success info to the sender of the picture
+
+        //before emit chack to which user to send event
+
+        //var tmpUserData = userData[socket.id];
+        //delete userData[socket.id];//privremeno brisem korisnika koji je poslao sliku da se ne bi i njemu poslolo obavestenje o novoj slici
+        //jer sigurno ispunjava kriterijum da je u blizini
+
+        var distance = 1001;
+        for(key in userData){
+          console.log(userData[socket.id].lat);
+
+          distance = getDistance(userData[socket.id].lat,userData[socket.id].lng,userData[key].lat,userData[key].lng);
+          console.log("Distance between user who send and user with socket.id:"+ key +" is:" + distance);
+
+          if(distance<1000.0 && key != socket.id )//ako je distanca manja od npr 1km onda se tom korisniku salje event da je slika dodata i ako to nije isti user koji je poslao tu sliku
+            //socket.broadcast.to(key).emit('newPictureAdded', "/images/temp.jpeg");
+            console.log("Send to socket.id:"+ key +" is:" + distance);
+            socket.broadcast.to(key).emit('newPictureAdded', "/images/temp.jpeg");//send to all user exept to the sender
+        }
+        //userData[socket.id] = tmpUserData;//vracanje usera koji je poslao sliku u listu usera koji su konektovani
       }
     });
 
@@ -55,6 +72,19 @@ io.on('connect', function(socket){
   });//end of addedNewPicture event
 
 });//end of connect event
+
+//Return distance between two coordinates
+function getDistance(lat1, lon1, lat2, lon2){
+  var R = 6378.137;
+  var dLat = (lat2 - lat1) * Math.PI / 180;
+  var dLon = (lon2 - lon1) * Math.PI / 180;
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+  Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+  Math.sin(dLon/2) * Math.sin(dLon/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c;
+  return d * 1000;
+}
 
 //End of Sockets
 function decodeBase64Image(dataString) {
